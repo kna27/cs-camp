2eb7d99cb1f0a75b43e65e8fab0339e066ef26e6
kna27 <krisharora27@gmail.com>
2023-09-01 14:58:48 -0400

    Move deleting images to utils

--- public/js/utils.js
+++ public/js/utils.js
@@ -25,3 +25,36 @@ const previewUploads = (inputId, previewContainerId) => {
         }
     });
 }
+
+const deleteImage = (deleteBtnClass, imgContainerId, family) => {
+    document.addEventListener('click', async function (e) {
+        if (e.target.classList.contains(deleteBtnClass)) {
+            e.preventDefault();
+            const imageName = e.target.getAttribute('data-image');
+            try {
+                if (family == null) {
+                    if (e.target.getAttribute('data-family') != null) {
+                        family = e.target.getAttribute('data-family');
+                    }
+                    else {
+                        console.error('Family not found');
+                        return;
+                    }
+                }
+                const response = await fetch(`/delete-image?family=${family}&image=${imageName}`, {
+                    method: 'DELETE'
+                });
+                if (response.ok) {
+                    const imageContainer = e.target.closest(`#${imgContainerId}`);
+                    if (imageContainer) {
+                        imageContainer.remove();
+                    }
+                } else {
+                    console.error('Failed to delete the image');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    });
+}
